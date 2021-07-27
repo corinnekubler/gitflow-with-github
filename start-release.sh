487c5cc508cc445bbcec061e4670130690098700
Corinne Kubler <corinne.kubler@symphony.com>
2021-07-27 17:02:41 +0200

    add script

--- start-release.sh
+++ start-release.sh
@@ -19,7 +19,7 @@ echo "Pull last changes"
 git pull
 
 echo "Start release with maven git flow plugin for version $VERSION"
-mvn gitflow:release-start
+mvn gitflow:release-start -DreleaseVersion $VERSION
 
 echo "Pushing release branch"
 git push
