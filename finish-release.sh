2b038cd58a805ee1e4f4ba04a69070b751ef220a
Corinne Kubler <corinne.kubler@symphony.com>
2021-07-27 18:30:54 +0200

    WIP

--- finish-release.sh
+++ finish-release.sh
@@ -5,6 +5,7 @@ mvn gitflow:release-finish
 
 
 git checkout master
+git pull
 RELEASE_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
 git reset --soft origin/master
 git checkout -b merge/release-to-master/$RELEASE_VERSION
@@ -13,6 +14,7 @@ git push --set-upstream origin merge/release-to-master/$RELEASE_VERSION
 
 
 git checkout develop
+git pull
 git reset --soft origin/develop
 git checkout -b merge/release-to-develop/$RELEASE_VERSION
 git commit -m "Changes for release $RELEASE_VERSION"
