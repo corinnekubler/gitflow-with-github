a3092742772aa7c923ef0cd2239fd15ef6f9b31d
Corinne Kubler <corinne.kubler@symphony.com>
2021-07-27 17:59:21 +0200

    WIP

--- finish-release.sh
+++ finish-release.sh
@@ -5,9 +5,9 @@ mvn gitflow:release-finish
 git checkout develop
 git reset --soft origin/develop
 git checkout -b merge/release-to-develop
-git push
+git push --set-upstream origin merge/release-to-develop
 
 git checkout master
-git reset --soft origin/develop
-git checkout -b merge/release-to-develop
-git push
+git reset --soft origin/master
+git checkout -b merge/release-to-master
+git push --set-upstream origin merge/release-to-master
