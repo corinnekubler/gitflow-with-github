9d419a47517c74304f57a361adb7eed380d465b9
Corinne Kubler <corinne.kubler@symphony.com>
2021-07-27 18:11:51 +0200

    WIP

--- finish-release.sh
+++ finish-release.sh
@@ -8,11 +8,11 @@ git checkout master
 RELEASE_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
 
 git reset --soft origin/master
-git checkout -b merge/release-to-master/RELEASE_VERSION
+git checkout -b merge/release-to-master/$RELEASE_VERSION
 git push --set-upstream origin merge/release-to-master
 
 
 git checkout develop
 git reset --soft origin/develop
-git checkout -b merge/release-to-develop/RELEASE_VERSION
+git checkout -b merge/release-to-develop/$RELEASE_VERSION
 git push --set-upstream origin merge/release-to-develop
