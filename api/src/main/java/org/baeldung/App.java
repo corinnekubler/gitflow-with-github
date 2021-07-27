72732cce2304dc005efa37ee0f6f0df2a8baefc4
Corinne Kubler <corinne.kubler@symphony.com>
2021-07-27 15:38:34 +0200

    feature 4

--- api/src/main/java/org/baeldung/App.java
+++ api/src/main/java/org/baeldung/App.java
@@ -4,10 +4,10 @@ package org.baeldung;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World with feature 4 commit 1!" );
     }
 }
