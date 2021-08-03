a63f56eea8e7a83aa9d334ebd071d3ffe71a8e73
Corinne Kubler <corinne.kubler@symphony.com>
2021-08-03 11:12:06 +0200

    release: changes for release 2.1

--- release-scripts/clean-release.sh
+++ release-scripts/clean-release.sh
@@ -3,6 +3,8 @@ echo "Please enter release to be cleaned: "
 read RELEASE_VERSION
 echo "RELEASE_VERSION: $RELEASE_VERSION"
 
+git checkout develop
+
 git branch -D merge/release-to-develop/$RELEASE_VERSION
 git branch -D merge/release-to-master/$RELEASE_VERSION
 git branch -D release/$RELEASE_VERSION
