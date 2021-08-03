f423ebfcfa1aeda1fe709029118fbeabc4435938
Corinne Kubler <corinne.kubler@symphony.com>
2021-08-03 09:57:10 +0200

    fix

--- release-scripts/clean-release.sh
+++ release-scripts/clean-release.sh
@@ -2,6 +2,12 @@
 echo "Please enter release to be cleaned: "
 read RELEASE_VERSION
 echo "RELEASE_VERSION: $RELEASE_VERSION"
+
+git branch -D merge/release-to-develop/$RELEASE_VERSION
+git branch -D merge/release-to-master/$RELEASE_VERSION
+git branch -D release/$RELEASE_VERSION
+
+
 git push origin --delete merge/release-to-develop/$RELEASE_VERSION
 git push origin --delete merge/release-to-master/$RELEASE_VERSION
 git push origin --delete release/$RELEASE_VERSION
