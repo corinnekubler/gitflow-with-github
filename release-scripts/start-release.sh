fb2adf7e4a2303ef9bd5540b2e7d629d512729ca
Corinne Kubler <corinne.kubler@symphony.com>
2021-08-04 10:44:46 +0200

    fix

---
+++ release-scripts/start-release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+echo "Starting release"
+
+echo "Checking out develop branch"
+git checkout develop
+echo "Pulling last changes"
+git pull
+
+echo "Starting release with maven git flow plugin"
+cd "$(dirname "$BASH_SOURCE")"/..
+mvn gitflow:release-start
+MVN_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
+RELEASE_VERSION=${MVN_VERSION//"-SNAPSHOT"/""}
+echo "RELEASE $RELEASE_VERSION"
+echo "Pushing release branch"
+git push --set-upstream origin release/$RELEASE_VERSION
+echo "To finish up the release process, run finish-release.sh"
+exit 0
